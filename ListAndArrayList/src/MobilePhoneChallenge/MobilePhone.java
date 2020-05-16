48b4c8093d8a90ce650ccd17d940f3de44d4e728
charles rummel <charles.g.rummel@gmail.com>
2020-05-16 14:34:20 -0500

    Add files via upload

--- ListAndArrayList/src/MobilePhoneChallenge/MobilePhone.java
+++ ListAndArrayList/src/MobilePhoneChallenge/MobilePhone.java
@@ -27,7 +27,12 @@ public class MobilePhone {
         if(foundPoistion < 0){
             System.out.println(oldContact.getName() + ", was not found.");
             return false;
+        } else if(findContact(newContact.getName()) != -1){
+            System.out.println("Contact with name " + newContact.getName() +
+                    " already exists. Update was not successful.");
+            return false;
         }
+
         this.myContacts.set(foundPoistion, newContact);
         System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
         return true;
